69479a0c84f5e0681ed2dd7b07ef2f5c8def042f
kiran sekhar panda <kiransekharp229@gmail.com>
2026-09-12 08:13:12 +0530

    push nodeDemo app to cicdDemo

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+    js.configs.recommended,
+
+    {
+        files: ["eslint.config.js"],
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+
+    {
+        files: ["src/**/*.js"],
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+
+    {
+        files: ["tests/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest
+            }
+        }
+    }
+];
