9433ae5a32ae9a5323fae0ef9ee2f00acee4751c
kiran sekhar panda <kiransekharp229@gmail.com>
2026-09-13 06:43:26 +0530

    push local to remote repository

---
+++ src/test/java/in/coderarmy/jenkinsSpringDemo/JenkinsSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.coderarmy.jenkinsSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
